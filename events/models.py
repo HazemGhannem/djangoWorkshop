65a15552ba9a29a300936ab36c9e6fd4e79a2878
HazemGhannem <hazem.ghannem@esprit.tn>
2023-02-03 16:56:53 +0100

    SuperUser

--- events/models.py
+++ events/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from users.models import Person
 
 # Create your models here.
+
 
 class Events(models.Model ):
     titre= models.CharField(max_length=50)
     descripton= models.TextField()
-    image= models.ImageField()
+    image= models.ImageField(upload_to='Image')
     CHOIX= (
         ('Musique','Musique'),
         ('Cinema','Cinema'),
@@ -17,5 +19,19 @@ class Events(models.Model ):
     evt_date= models.DateField()
     created_at= models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    #relation 
+    organize = models.ForeignKey(Person,on_delete=models.CASCADE)
+    Participation = models.ManyToManyField(
+        Person,
+        through='Participation',
+        related_name='participations'
+
+    )
+
+
+class Participation(models.Model):
+    Person = models.ForeignKey(Person,on_delete=models.CASCADE)
+    event = models.ForeignKey(Events,on_delete=models.CASCADE)
+    date_participation = models.DateTimeField(auto_now_add=True)
 
 
--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class Person(AbstractUser):
+    cin =models.CharField(primary_key=True,max_length=8)
+    email = models.EmailField(unique=True)
+    username = models.CharField(unique=True,max_length=40)
+    USERNAME_FIELD ='username'
+    
